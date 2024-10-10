948237c65d95a8914930f49de15afe6352e942e6
Ivan <holguinivan001@gmail.com>
2024-10-09 20:33:07 -0500

    Added password reset functionality

--- game_project/news/urls.py
+++ game_project/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='news-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('main/', PostListView.as_view(), name='news-main'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
